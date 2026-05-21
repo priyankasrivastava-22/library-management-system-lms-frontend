0fb019ff37d29b6e98313d7d54d80e8d5cedac12
priyankasrivastava-22 <priyankarenu20@gmail.com>
2026-05-21 11:42:13 +0000

    Improve Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,7 +32,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("`${API_URL}/api/auth/login", {
+      const res = await fetch(`${API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
